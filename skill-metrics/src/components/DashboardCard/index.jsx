1fe5721c58bc3373a207fb1b162328a48a148e09
Lakshmy Venkitaraman <lakshmy.venkitaraman@terrificminds.com>
2025-08-12 11:13:21 +0530

    added sidebar,header,dashboard

--- skill-metrics/src/components/DashboardCard/index.jsx
+++ skill-metrics/src/components/DashboardCard/index.jsx
@@ -1,11 +1,15 @@
-export default function DashboardCard({ title, value, icon, color }) {
+import React from "react";
+
+export default function DashboardCard({ title, value, icon = null,  gradientClass }) {
   return (
-    <div className={`p-4 rounded-xl shadow-md text-white ${color}`}>
-    <div className="flex items-center gap-3">
-    {icon && <span className="text-2xl">{icon}</span>}
-    <div>
-        <h3 className="text-sm">{title}</h3>
-        <p className="text-2xl font-bold">{value}</p>
+     <div className={`relative overflow-hidden rounded-xl p-6 text-white ${gradientClass}`}> 
+     <div className="relative  flex items-center justify-between">
+        <div>
+          <p className="text-sm opacity-80">{title}</p>
+          <p className="mt-1 text-3xl font-bold ">{value}</p>
+        </div>
+        <div className="flex h-12 w-12 items-center justify-center ">
+          {icon}
         </div>
       </div>
     </div>
--- skill-metrics/src/components/Sidebar.jsx
+++ skill-metrics/src/components/Sidebar.jsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiGrid} from "react-icons/fi";
+import { BsGraphUpArrow } from "react-icons/bs";
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-white border-r">
-    <div className="p-4 font-bold text-orange-500">Skill Metrics</div>
-    <nav className="flex flex-col p-2 space-y-2">
-    <Link to="/" className="p-2 rounded hover:bg-orange-100">Dashboard</Link>
-    <Link to="/skill-profile" className="p-2 rounded hover:bg-orange-100">Skill Profile</Link>
-    </nav>
+    <aside className="w-64 h-screen bg-white text-black shadow-lg flex flex-col">
+  <div className="flex items-center p-4 text-lg font-bold border-b">
+  <img className="w-12 h-12 object-cover mr-3" src="./src/assets/download.png" alt=" Logo"/>
+  <p className="text-black font-medium">SkillMetrics</p>
+</div>
+
+      <nav className="mt-4 flex flex-col gap-1 px-2">
+  <a href="/" className="flex items-center gap-3 rounded-lg px-4 py-3 text-sm font-medium hover:bg-orange-300">
+    <FiGrid className="h-6 w-6" />Dashboard
+  </a>
+  <a href="/profile" className="flex items-center gap-3 rounded-lg px-4 py-3 text-sm font-medium hover:bg-orange-300" >
+    <BsGraphUpArrow className="h-6 w-6" />SkillProfile
+  </a>
+</nav>
+
     </aside>
-  )
+  );
 }
